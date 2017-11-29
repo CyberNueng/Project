ebec2213c201f870edaa6ea6c8a584e33a66f020
CyberNueng <nueng.42560@gmail.com>
2017-11-29 17:20:01 +0700

    remove title bar

--- app/src/main/java/cs355/project/MainActivity.java
+++ app/src/main/java/cs355/project/MainActivity.java
@@ -2,6 +2,8 @@ package cs355.project;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
@@ -9,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
         LinearLayout layout =(LinearLayout)findViewById(R.id.background);
         layout.setBackgroundResource(R.drawable.background);
