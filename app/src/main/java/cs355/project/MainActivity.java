8ca2a05be8532bcb02212e0ba4be99837a64a13b
cupimpat <cupimpat@hotmail.com>
2017-11-29 22:01:00 +0700

    have a background problem
    nothing
    HELPPPPPPPPPPPPPPPPPPPPPPP!!!!!!!!!!

--- app/src/main/java/cs355/project/MainActivity.java
+++ app/src/main/java/cs355/project/MainActivity.java
@@ -1,9 +1,14 @@
 package cs355.project;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,9 +16,20 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_main2);
         LinearLayout layout =(LinearLayout)findViewById(R.id.background);
         layout.setBackgroundResource(R.drawable.background);
+
+        ImageButton imageButton = (ImageButton) findViewById(R.id.imgbutton);
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,
+                        MainActivity2.class);
+
+                startActivity(intent);
+            }
+        });
     }
 }
