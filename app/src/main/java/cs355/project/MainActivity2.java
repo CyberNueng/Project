accfc37bfe772a1902222fa92f3e2ba6f817b3e8
CyberNueng <nueng.42560@gmail.com>
2017-11-29 16:44:34 +0700

    add backgroung

--- app/src/main/java/cs355/project/MainActivity2.java
+++ app/src/main/java/cs355/project/MainActivity2.java
@@ -2,6 +2,7 @@ package cs355.project;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.RelativeLayout;
 
 public class MainActivity2 extends AppCompatActivity {
 
@@ -9,5 +10,7 @@ public class MainActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        RelativeLayout layout =(RelativeLayout)findViewById(R.id.background);
+        layout.setBackgroundResource(R.drawable.background);
     }
 }
