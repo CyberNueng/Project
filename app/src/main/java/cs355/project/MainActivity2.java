ddcdfe71a3825d959b55531ebcf8767064ca528c
CyberNueng <nueng.42560@gmail.com>
2017-12-01 04:34:22 +0700

    add pic and senser

--- app/src/main/java/cs355/project/MainActivity2.java
+++ app/src/main/java/cs355/project/MainActivity2.java
@@ -1,13 +1,26 @@
 package cs355.project;
 
+import android.content.BroadcastReceiver;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.LinearLayout;
 
-public class MainActivity2 extends AppCompatActivity {
-
+public class MainActivity2 extends AppCompatActivity implements SensorEventListener {
+    SensorManager sensorManager;
+    Handler hdr = new Handler();
+    float acc_x, acc_y, acc_z;
+    int POLL_INTERVAL = 500;
+    int shake_threshold = 30;
+    BroadcastReceiver batteryInfoReceiver;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,5 +28,49 @@ public class MainActivity2 extends AppCompatActivity {
         setContentView(R.layout.activity_main2);
         LinearLayout layout =(LinearLayout)findViewById(R.id.background);
         layout.setBackgroundResource(R.drawable.background);
+        sensorManager=(SensorManager)getSystemService(SENSOR_SERVICE);
+        sensorManager.registerListener((SensorEventListener) this,
+                sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
+                SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    public void onSensorChanged(SensorEvent event){
+        int type = event.sensor.getType();
+        if(type==Sensor.TYPE_ACCELEROMETER){
+            acc_x=event.values[0];
+            acc_y=event.values[1];
+            acc_z=event.values[2];
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
+    }
+
+    private Runnable pollTask = new Runnable() {
+        public void run() {
+            hdr.postDelayed(pollTask,POLL_INTERVAL);
+        }
+    };
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        sensorManager.registerListener(this, sensorManager.getDefaultSensor(
+                Sensor.TYPE_ACCELEROMETER),
+                SensorManager.SENSOR_DELAY_NORMAL);
+        sensorManager.registerListener(this, sensorManager.getDefaultSensor(
+                Sensor.TYPE_AMBIENT_TEMPERATURE),
+                SensorManager.SENSOR_DELAY_NORMAL);
+        this.registerReceiver(this.batteryInfoReceiver, new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
+        hdr.postDelayed(pollTask, POLL_INTERVAL);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(this);
+        hdr.removeCallbacks(pollTask);
     }
 }
