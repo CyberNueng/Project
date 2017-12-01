2274648a0dd40e0c592367c5a83497996c721498
CyberNueng <nueng.42560@gmail.com>
2017-12-01 22:10:12 +0700

    update

--- app/src/main/java/cs355/project/MainActivity2.java
+++ app/src/main/java/cs355/project/MainActivity2.java
@@ -1,6 +1,7 @@
 package cs355.project;
 
 import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.hardware.Sensor;
@@ -8,6 +9,7 @@ import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Handler;
+import android.os.Vibrator;
 import android.support.v7.app.AppCompatActivity;
 import android.app.Activity;
 import android.os.Bundle;
@@ -15,6 +17,7 @@ import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.view.animation.TranslateAnimation;
+import android.view.animation.Animation;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
@@ -27,9 +30,10 @@ public class MainActivity2 extends Activity implements SensorEventListener {
     Handler hdr = new Handler();
     float acc_x, acc_y, acc_z;
     int POLL_INTERVAL = 500;
-    int shake_throw = 20;
+    int shake_throw = 15;
     int shake_take = 10;
     int pause_shake = 5;
+    int numberThrow = 0;
     BroadcastReceiver batteryInfoReceiver;
     ArrayList<ImageView> list = new ArrayList<ImageView>();
     TranslateAnimation ta, ta1, ta2, ta3, ta4, ta5, ta6;
@@ -37,6 +41,10 @@ public class MainActivity2 extends Activity implements SensorEventListener {
     int stage = 1; //number of pokemon troll
     int floor = 1450;
     int deadline = 1300;
+    Vibrator v;
+    int x[] = new int[5];
+    int y[] = new int[5];
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -44,6 +52,7 @@ public class MainActivity2 extends Activity implements SensorEventListener {
         setContentView(R.layout.activity_main2);
         FrameLayout layout =(FrameLayout)findViewById(R.id.background);
         layout.setBackgroundResource(R.drawable.background);
+        v = (Vibrator)this.getSystemService(Context.VIBRATOR_SERVICE);
         sensorManager=(SensorManager)getSystemService(SENSOR_SERVICE);
         sensorManager.registerListener((SensorEventListener) this,
                 sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
@@ -95,7 +104,11 @@ public class MainActivity2 extends Activity implements SensorEventListener {
     public void play(){
         if(state==0)
         {
-
+            if( (Math.abs(acc_x)>shake_throw) || (Math.abs(acc_y)>shake_throw) || (Math.abs(acc_z)>shake_throw) ) {
+                v.vibrate(500);
+                throwPoke();
+                state=1;
+            }
         }
         else if(state==1)
         {
@@ -108,8 +121,18 @@ public class MainActivity2 extends Activity implements SensorEventListener {
     }
 
     public void throwPoke(){
+        Random r = new Random();
         if(stage==1){
-            
+            int rand_x2 = r.nextInt(800);
+            int rand_y2 = r.nextInt(200)-400;
+            ta = new TranslateAnimation(x[numberThrow], rand_x2, floor, rand_y2);
+            int rand_speed = r.nextInt(200)+400;
+            ta.setDuration(rand_speed);
+            ta.setFillAfter(true);
+            list.get(numberThrow).startAnimation(ta);
+            x[numberThrow] = rand_x2;
+            y[numberThrow] = rand_y2;
+            numberThrow++;
         }
     }
 
@@ -124,10 +147,11 @@ public class MainActivity2 extends Activity implements SensorEventListener {
         list.remove(0).setVisibility(View.GONE);
         for(int i = 0; i<5; i++) {
             Random r = new Random();
-            int rand_x = r.nextInt(800);
+            x[i] = r.nextInt(800);
+            y[i] = floor;
             int rand_y = r.nextInt(500);
             int rand_x2 = r.nextInt(800);
-            ta = new TranslateAnimation(rand_x, rand_x2, rand_y, floor);
+            ta = new TranslateAnimation(x[i], rand_x2, rand_y, floor);
             int rand_drop = r.nextInt(400)+800;
             ta.setDuration(rand_drop);
             ta.setFillAfter(true);
