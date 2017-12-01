a438843a6069c40f0b549d3b74ec40f2f62b92e4
CyberNueng <nueng.42560@gmail.com>
2017-12-02 02:48:16 +0700

    update

--- app/src/main/java/cs355/project/MainActivity2.java
+++ app/src/main/java/cs355/project/MainActivity2.java
@@ -33,7 +33,7 @@ public class MainActivity2 extends Activity implements SensorEventListener {
     int shake_throw = 15;
     int shake_take = 10;
     int pause_shake = 5;
-    int numberThrow = 0;
+    int canTake = 0;
     BroadcastReceiver batteryInfoReceiver;
     ArrayList<ImageView> list = new ArrayList<ImageView>();
     TranslateAnimation ta, ta1, ta2, ta3, ta4, ta5, ta6;
@@ -44,6 +44,7 @@ public class MainActivity2 extends Activity implements SensorEventListener {
     Vibrator v;
     int x[] = new int[5];
     int y[] = new int[5];
+    boolean pause = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,6 +53,8 @@ public class MainActivity2 extends Activity implements SensorEventListener {
         setContentView(R.layout.activity_main2);
         FrameLayout layout =(FrameLayout)findViewById(R.id.background);
         layout.setBackgroundResource(R.drawable.background);
+        LinearLayout pauseLayout = (LinearLayout)findViewById(R.id.pause);
+        pauseLayout.setVisibility(LinearLayout.INVISIBLE);
         v = (Vibrator)this.getSystemService(Context.VIBRATOR_SERVICE);
         sensorManager=(SensorManager)getSystemService(SENSOR_SERVICE);
         sensorManager.registerListener((SensorEventListener) this,
@@ -102,7 +105,7 @@ public class MainActivity2 extends Activity implements SensorEventListener {
     }
 
     public void play(){
-        if(state==0)
+        if(state==0&&!pause)
         {
             if( (Math.abs(acc_x)>shake_throw) || (Math.abs(acc_y)>shake_throw) || (Math.abs(acc_z)>shake_throw) ) {
                 v.vibrate(500);
@@ -110,29 +113,36 @@ public class MainActivity2 extends Activity implements SensorEventListener {
                 state=1;
             }
         }
-        else if(state==1)
+        else if(state==1&&!pause)
         {
 
         }
-        else
+        else if(!pause)
         {
+
+        }
+        if(stage==4&&list.size()==0){
 
         }
     }
 
     public void throwPoke(){
         Random r = new Random();
-        if(stage==1){
-            int rand_x2 = r.nextInt(800);
-            int rand_y2 = r.nextInt(200)-400;
-            ta = new TranslateAnimation(x[numberThrow], rand_x2, floor, rand_y2);
-            int rand_speed = r.nextInt(200)+400;
-            ta.setDuration(rand_speed);
-            ta.setFillAfter(true);
-            list.get(numberThrow).startAnimation(ta);
-            x[numberThrow] = rand_x2;
-            y[numberThrow] = rand_y2;
-            numberThrow++;
+        int rand_x2 = r.nextInt(800);
+        int rand_y2 = r.nextInt(200)-400;
+        ta = new TranslateAnimation(x[0], rand_x2, floor, rand_y2);
+        int rand_speed = r.nextInt(200)+400;
+        ta.setDuration(rand_speed);
+        ta.setFillAfter(true);
+        list.get(0).startAnimation(ta);
+        x[0] = rand_x2;
+        y[0] = rand_y2;
+
+        switch(stage){
+            case 1: canTake=1;
+            case 2: canTake=2;
+            case 3: canTake=3;
+            case 4: canTake=4;
         }
     }
 
@@ -145,6 +155,7 @@ public class MainActivity2 extends Activity implements SensorEventListener {
         list.add((ImageView)findViewById(R.id.img6));
         Collections.shuffle(list);
         list.remove(0).setVisibility(View.GONE);
+        setBtn();
         for(int i = 0; i<5; i++) {
             Random r = new Random();
             x[i] = r.nextInt(800);
@@ -158,5 +169,62 @@ public class MainActivity2 extends Activity implements SensorEventListener {
             list.get(i).setAnimation(ta);
             ta.start();
         }
+    }
+
+    public void pause(){
+
+    }
+
+    public void unpuase(){
+
+    }
+
+    public void setBtn(){
+        list.get(0).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(canTake>0) {
+                    canTake--;
+                    v.setVisibility(View.GONE);
+                }
+            }
+        });
+        list.get(1).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(canTake>0) {
+                    canTake--;
+                    v.setVisibility(View.GONE);
+                }
+            }
+        });
+        list.get(2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if(canTake>0) {
+                    canTake--;
+                    v.setVisibility(View.GONE);
+                }
+            }
+        });
+        list.get(3).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(canTake>0) {
+                    canTake--;
+                    v.setVisibility(View.GONE);
+                }
+            }
+        });
+        list.get(4).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(canTake>0) {
+                    canTake--;
+                    v.setVisibility(View.GONE);
+                }
+            }
+        });
     }
 }
