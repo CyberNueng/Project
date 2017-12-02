e6b1749551e7db9666b613a4d2d6eeacea9a592a
cupimpat <cupimpat@hotmail.com>
2017-12-02 13:50:15 +0700

    p4

---
+++ app/src/main/java/cs355/project/MainActivity4.java
@@ -0,0 +1,31 @@
+package cs355.project;
+
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+
+public class MainActivity4 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main4);
+        ImageButton ib = (ImageButton)findViewById(R.id.dead);
+        ib.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity4.this,
+                        MainActivity2.class);
+
+                startActivity(intent);
+            }
+        });
+        MediaPlayer song = MediaPlayer.create(MainActivity4.this, R.raw.win);
+        song.setLooping(false);
+        song.start();
+    }
+}
